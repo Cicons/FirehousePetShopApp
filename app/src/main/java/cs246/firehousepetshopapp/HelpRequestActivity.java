d9bb8e26abbe54b23fa71ff55a233549dc576a84
c8t12h19 <hol12062@byui.edu>
2017-06-10 12:09:45 -0600

    Added all Activity pages so I can edit their features.

---
+++ app/src/main/java/cs246/firehousepetshopapp/HelpRequestActivity.java
@@ -0,0 +1,11 @@
+package cs246.firehousepetshopapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cutlerhollist on 6/10/17.
+ */
+
+public class HelpRequestActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/cs246/firehousepetshopapp/ViewHelpRequestActivity.java
@@ -0,0 +1,10 @@
+package cs246.firehousepetshopapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cutlerhollist on 6/10/17.
+ */
+
+public class ViewHelpRequestActivity extends AppCompatActivity {
+}
