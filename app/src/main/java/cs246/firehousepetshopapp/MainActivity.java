8b40cad61e3c92a8297e97f39dc99d3c258c2fde
c8t12h19 <hol12062@byui.edu>
2017-06-28 12:56:49 -0600

    Figured out Splash Screen, still have to fix SharedPreferences.

--- app/src/main/java/cs246/firehousepetshopapp/MainActivity.java
+++ app/src/main/java/cs246/firehousepetshopapp/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
@@ -38,67 +37,94 @@ public class MainActivity extends AppCompatActivity {
         // Practice setting up SharedPreferences with a fake Customer - this just follows the same
         //   code as in our reading quiz. I have no idea if it works.
         // THIS SHOULD BE USED in onDestroy() for the sign in activity
-        Customer bob = new Customer("Bob"); // look up "constructor chain" for Customer
-        String key = "Customer Username";
-        Context context = this; // you CAN just put "this" inside the parentheses in the line below,
-                                //   but I want to show that "this" (MainActivity) is subclassed to Context
+
+        // SharedPreferences saved at end of SignInActivity, need a static "key" and "value", I think
+        String key = "Bob";
+        Customer c = new Customer(key);
+        final Context context = this;
+
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor prefEditor = pref.edit();
         prefEditor.apply();
-        prefEditor.putString(key, bob.getUserName());
+        prefEditor.putString(key, c.getUserName());
         prefEditor.commit();
 
-        // THIS SHOULD BE USED in onCreate() in the MainEmptyActivity that will determine if there
-        //   is a Username already saved and will then bring up the screen for sign in or the specific
-        //   user's main activity.
-        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-        if (sharedPref.getString(key, bob.getUserName()) != null)
-        {
-             //use Firebase to determine if the person is a manager or customer
+        // THIS will determine if there is a Username already saved and will then bring up the
+        //   screen for sign in or the specific user's main activity.
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
 
-            // and then open their specific main activity
-            if() {
-                Intent openCustomerMainIntent = new Intent(this, CustomerMainActivity.class);
-                startActivity(openCustomerMainIntent);
-            }
-            else if() {
-                Intent openManagerMainIntent = new Intent(this, ManagerMainActivity.class);
-                startActivity(openManagerMainIntent);
-            }
 
-        }
-        else
-        {
-            Intent openSignInIntent = new Intent(this, SignInActivity.class);
-            startActivity(openSignInIntent);
-        }
+                    SharedPreferences sharedPref = context.getPreferences(Context.MODE_PRIVATE);
+
+                    if (sharedPref.getString(key, c.getUserName()) != null)
+                    {
+                        //use Firebase to determine if the person is a manager or customer
+
+                        // and then open their specific main activity
+                        if() { // bob is a customer
+                            Intent openCustomerMainIntent = new Intent(context, CustomerMainActivity.class);
+                            startActivity(openCustomerMainIntent);
+                            finish();
+                        }
+                        else if() { // bob is a manager
+                            Intent openManagerMainIntent = new Intent(context, ManagerMainActivity.class);
+                            startActivity(openManagerMainIntent);
+                            finish();
+                        }
+
+                    }
+                    else
+                    {
+                        Intent openSignInIntent = new Intent(context, SignInActivity.class);
+                        startActivity(openSignInIntent);
+                        finish();
+                    }
+
+
+
+                    /* I think the following code needs to be tested in another activity, that way
+                    *  we can test the above code and finish the activity, then move to the next
+                    *  activity and try sending something up in that one.*/
+
+                    // Write a message to the database
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("message");
+
+                    myRef.setValue("Hello, World!");
+                    // Read from the database
+                    myRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            //    whenever data at this location is updated.
+                            String value = dataSnapshot.getValue(String.class);
+                            Log.d(TAG, "Value is: " + value);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+    }
+}
+
+
 
 
 //        int defaultValue = getResources().getInteger(R.string.saved_high_score_default);
 //        long highScore = sharedPref.getInt(getString(R.string.saved_high_score), defaultValue);
 
-
-
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                //    whenever data at this location is updated.
-        String value = dataSnapshot.getValue(String.class);
-        Log.d(TAG, "Value is: " + value);
-     }
-
-    @Override
-    public void onCancelled(DatabaseError error) {
-    // Failed to read value
-    Log.w(TAG, "Failed to read value.", error.toException());
-        }
-        });
-    }
-}
