ece315009f1682e2ca866f8a153c90d236e6a10f
Payaso55 <lukehagen517@gmail.com>
2017-06-27 20:18:37 -0600

    barcode code

--- app/src/main/java/cs246/firehousepetshopapp/QRObject.java
+++ app/src/main/java/cs246/firehousepetshopapp/QRObject.java
@@ -1,5 +1,10 @@
 package cs246.firehousepetshopapp;
 
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
 /**
  * Created by sam on 6/26/2017.
  */
@@ -37,4 +42,13 @@ public class QRObject {
     public void setPosValue(boolean posValue) {
         this.posValue = posValue;
     }
+
+    try {
+        // generate a 150x150 QR code
+        Bitmap bm = encodeAsBitmap(barcode_content, BarcodeFormat.QR_CODE, 150, 150);
+
+        if(bm != null) {
+            image_view.setImageBitmap(bm);
+        }
+    } catch (WriterException e) { //eek }
 }
