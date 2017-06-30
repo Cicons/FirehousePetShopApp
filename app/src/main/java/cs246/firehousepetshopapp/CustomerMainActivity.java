eda374921a0433963b257caad363afe39eba8a5f
Taelement <cic14002@byui.edu>
2017-06-30 15:10:32 -0600

    Added implimentation for the reward listView

--- app/src/main/java/cs246/firehousepetshopapp/CustomerMainActivity.java
+++ app/src/main/java/cs246/firehousepetshopapp/CustomerMainActivity.java
@@ -2,6 +2,10 @@ package cs246.firehousepetshopapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by cutlerhollist on 6/10/17.
@@ -9,8 +13,39 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CustomerMainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // call for array of rewards from firebase
+        // Convert array of Rewards to array of Strings
+
+        Reward[] rewArray = {};
+
+        ArrayList<String> rewList = new ArrayList<String>();
+
+        for(int i = 0; i < rewArray.length; i++)
+        {
+            String title = rewArray[i].getTitle();
+            String cost = Integer.toString(rewArray[i].getCost());
+            String description = rewArray[i].getDescription();
+
+            rewList.add(title + " -- cost: " + cost + " points" + "\n" + description);
+        }
+
+        //sam's listView code:
+
+     // basic listView set-up
+     ArrayAdapter adapter1 = new ArrayAdapter<String>(
+             this,  // Context for activity
+             android.R.layout.simple_list_item_1,  // Layout to use
+             rewList  // items to be displayed
+     );
+     ListView mainListView = (ListView) findViewById(R.id.expandableListView2);
+     mainListView.setAdapter(adapter1);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
     }
