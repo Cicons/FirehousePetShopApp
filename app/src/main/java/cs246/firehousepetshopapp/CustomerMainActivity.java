1987afc31bfc5b6f6db3ad8e1432341c3008521b
Payaso55 <lukehagen517@gmail.com>
2017-07-11 18:01:20 -0600

    Customer error free

--- app/src/main/java/cs246/firehousepetshopapp/CustomerMainActivity.java
+++ app/src/main/java/cs246/firehousepetshopapp/CustomerMainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class CustomerMainActivity extends AppCompatActivity {
     @Override    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
@@ -15,11 +15,10 @@ public class MainActivity extends AppCompatActivity {
         String[] items2 = {"A", "B", "C"};
         ArrayList<String> combItems = new ArrayList<String>();
         for(int i = 0; i < items1.length; i++) {
-    }
-    for (int c = 0; c < items2.length; c++) {
-        combItems.add(items1[i] + ": " + items2[c]);
-    }
-    }
+            for (int c = 0; c < items2.length; c++) {
+                combItems.add(items1[i] + ": " + items2[c]);
+            }
+        }
     ArrayAdapter adapter1 = new ArrayAdapter<String>(
             this, android.R.layout.simple_list_item_1, items1);
                  ListView theListView = (ListView) findViewById(R.id.mainListView);
